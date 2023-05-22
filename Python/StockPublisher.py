094868ba8e29ad4829700f15ec84e7c91bd0b648
Sophie Tao <121151894+sophietao127@users.noreply.github.com>
2023-05-21 23:44:03 -0700

    Add more markets and stocks

--- Python/StockPublisher.py
+++ Python/StockPublisher.py
@@ -12,8 +12,9 @@ publish message from StockMarket
 def main(natsurl):
     # get the message from Stock Market
     print("Starting stock publisher....")
-    sm1 = StockMarket(["AMZN", "GOOG", "MSFT"])
-    sm2 = StockMarket(["GE", "GMC", "FORD"])
+    sm1 = StockMarket(["AMZN", "GOOG", "MSFT", "ADOBE", "EXPEDIA", "HEALTH", "CARE"])
+    sm2 = StockMarket(["GE", "GMC", "FORD", "TESLA", "BOEING", "GLASSDOOR", "LINKEDIN"])
+    sm3 = StockMarket(["APPLE", "OKTA", "NETFLX", "HANDSHAKE", "UW", "SONOS", "OPPO"])
 
     with NATSClient(url=natsurl) as nc:
         nc.connect()
@@ -23,6 +24,7 @@ def main(natsurl):
             time.sleep(2)
             thread1 = symbolThread(sm1)
             thread2 = symbolThread(sm2)
+            thread3 = symbolThread(sm3)
             # start the thread for 1st market
             thread1.start()
             thread1.join()                  # wait for the thread to finish
@@ -60,9 +62,25 @@ def main(natsurl):
             # Publish a message
             # nc.publish(subject=subject1, payload=bytes(message1, "UTF-8"));
             nc.publish(subject=subject2, payload=message2);
-            # print("Exiting main...")
+
+            # start the thread for 3rd market
+            thread3.start()
+            thread3.join()                  # wait for the thread to finish
+            data3 = thread3.return_val      # get the value returned from the thread
+            print(data3[0], data3[1], data3[2], data3[3])
+            subject3, adjust3, price3 = data3[0], str(data3[1]), str(data3[2])
+            time3 = "'{t}'".format(t=data3[3])
+            message3 = '''<?xml version="1.0" encoding="UTF-8"?>
+                            <message sent={time}>
+                            <stock>
+                                <name>{symbol}</name>
+                                <adjustment>{adjust}</adjustment>
+                                <adjustedPrice>{price}</adjustedPrice>
+                            </stock>
+                            </message>
+                        '''.format(time=time3, symbol=subject3, adjust=adjust3, price=price3)
+            nc.publish(subject=subject3, payload=message3);
 
 natsurl = "nats://localhost:4222"
 print("Starting Python message producer....")
 main(natsurl)
-
